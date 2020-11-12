f34e36df77e3de6231810e4e4e5bf012067b9315
aikmanj24 <71267711+aikmanj24@users.noreply.github.com>
2020-11-11 20:22:28 -0600

    Add files via upload

---
+++ j3.js
@@ -0,0 +1,11 @@
+var termProject = "keyword";
+<p id="demo"></p>
+
+<script>
+var termProject = "keyword";
+document.getElementById("demo").innerHTML = termProject;
+</script>
+.classList.add('animate"termProject"animated', 'animate"termProject"backInRight', 'animate"termProjectslower');
+<script>
+console.log("AnimationComplete");
+</script> 
